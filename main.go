45b6f50f667320423b04e239a07fa8303c0da371
0x2170 <151378415+DLLenjoyer@users.noreply.github.com>
2024-06-01 17:36:09 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/DLLenjoyer/books-api/internal/handlers"
+	"github.com/DLLenjoyer/books-api/internal/repository"
+	"github.com/DLLenjoyer/books-api/internal/service"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	repo := repository.NewInMemoryBook()
+	bookService := service.NewBookService(repo)
+	bookHandler := handlers.NewBookHandler(bookService)
+
+	r := mux.NewRouter()
+	bookHandler.SetupRoutes(r)
+
+	log.Println("starting server on :6030")
+	log.Fatal(http.ListenAndServe(":6030", r))
+}
