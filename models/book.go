61a3c48cd380395bb01659ea82d13cc27f91e17b
0x2170 <151378415+DLLenjoyer@users.noreply.github.com>
2024-06-01 16:48:18 +0200

    Update book.go

--- models/book.go
+++ models/book.go
@@ -1,9 +1,9 @@
 package models
 
 type Book struct {
-	ID     string `json:"id"`
+    ID     string `json:"id"`
     Title  string `json:"title"`
     Author string `json:"author"`
     Year   int    `json:"year"`
-	Price float64 `json:"price"`
+    Price float64 `json:"price"`
 }
